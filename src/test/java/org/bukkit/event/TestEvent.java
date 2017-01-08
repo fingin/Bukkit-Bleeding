5e5b34a893a76fd2c86bc8fe6ac01e520099f8e5
fingin <albertlabonteiii3@gmail.com>
2017-01-08 08:58:08 -0700

    Update TestEvent.java

--- src/test/java/org/bukkit/event/TestEvent.java
+++ src/test/java/org/bukkit/event/TestEvent.java
@@ -4,8 +4,8 @@ package org.bukkit.event;
 public class TestEvent extends Event {
     private static final HandlerList handlers = new HandlerList();
 
-    public TestEvent(boolean async) {
-        super(async);
+    public TestEvent(boolean sync) {
+        super(sync);
     }
 
     @Override
